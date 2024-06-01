448e7d3666aaec557cc51bb52897d7c426c31199
Abhishek Ranjan <142088316+Abhishek181003@users.noreply.github.com>
2024-06-01 19:40:07 +0530

    Update Upper_Bound.java

--- Binary Search/Upper_Bound.java
+++ Binary Search/Upper_Bound.java
@@ -15,7 +15,7 @@ class Upper_Bound {
         int upperBound = n;
         int lo = 0 , hi = n-1;
         while(lo<=hi){
-            int mid = lo + hi-lo/2;
+            int mid = lo + (hi-lo)/2;
             if(arr[mid]>target){
                 upperBound = Math.min(upperBound,mid);
                 hi= mid-1;
