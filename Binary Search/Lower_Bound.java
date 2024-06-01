8bb87b37349b752933c6350fac883814834754d3
Abhishek Ranjan <142088316+Abhishek181003@users.noreply.github.com>
2024-06-01 17:43:04 +0530

    Update and rename Lower Bound.java to Lower_Bound.java

--- Binary Search/Lower_Bound.java
+++ Binary Search/Lower_Bound.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class HelloWorld {
+class Lower_Bound {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array ");
