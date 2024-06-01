268e068f3a2e811b936185e5abb912cadd1987d8
Abhishek Ranjan <142088316+Abhishek181003@users.noreply.github.com>
2024-06-01 19:39:44 +0530

    Update Lower_Bound.java

--- Binary Search/Lower_Bound.java
+++ Binary Search/Lower_Bound.java
@@ -17,7 +17,7 @@ class Lower_Bound {
         int lowerBound = n;
         int lo = 0 , hi = n-1;
         while(lo<=hi){
-            int mid = lo + hi-lo/2;
+            int mid = lo + (hi-lo)/2;
             if(arr[mid]>=target){
                 lowerBound = Math.min(lowerBound,mid);
                 hi= mid-1;
