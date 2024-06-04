dfcdac0c347e09efd1287a6c7985085cfeacd8a8
Abhishek Ranjan <142088316+Abhishek181003@users.noreply.github.com>
2024-06-04 15:12:25 +0530

    Update Pattern.java

--- Reccursion/Pattern.java
+++ Reccursion/Pattern.java
@@ -2,11 +2,11 @@ import java.util.*;
 class Pattern {
    public static void pip(int n){
        if(n==0) return;
-       System.out.print(n+" ");
+       System.out.print(n+" "); // pre
        pip(n-1);
-       System.out.print(n+" ");
+       System.out.print(n+" "); // in
        pip(n-1);
-       System.out.print(n+" ");
+       System.out.print(n+" "); // post
    }
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
