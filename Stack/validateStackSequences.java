8b5f01abcd33d42cfa15336d91e87a15ff95d8bc
Abhishek Ranjan <142088316+Abhishek181003@users.noreply.github.com>
2024-06-24 05:12:43 +0530

    Update validateStackSequences.java
    Example 1:
    
    Input: pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
    Output: true
    Explanation: We might do the following sequence:
    push(1), push(2), push(3), push(4),
    pop() -> 4,
    push(5),
    pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1
    Example 2:
    
    Input: pushed = [1,2,3,4,5], popped = [4,3,5,1,2]
    Output: false
    Explanation: 1 cannot be popped before 2.

--- Stack/validateStackSequences.java
+++ Stack/validateStackSequences.java
@@ -2,7 +2,7 @@ class Solution {
   public boolean validateStackSequences(int[] pushed, int[] popped) {
         int n = pushed.length;
         Stack <Integer> st = new Stack<>();
-        int idx = 0; // to iterate over popped array
+        int idx = 0; // to iterate over popped array 
         for(int i=0; i<n; i++){ // to iterate over pushed array
             st.push(pushed[i]);
             if(pushed[i] == popped[idx]){
